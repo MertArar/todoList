538f0631a2ba2a8653d83f91a3bb73c9bf136432
MertArar <85193329+MertArar@users.noreply.github.com>
2023-06-05 05:29:46 +0300

    bug fixed

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const Heading = () => {
   return (
     <div className="text-center text-4xl font-bold">
-      Mert's ToDoList. <br /> <span className="text-sky-700">Enjoy!</span>
+      Mert's ToDoList. <br />{" "}
+      <div className="mt-4 mb-2">
+        {" "}
+        <span className="text-sky-700 mt-56">Enjoy!</span>{" "}
+      </div>
     </div>
   );
 };
