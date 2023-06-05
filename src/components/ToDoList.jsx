7624a97434689ca43df8bcaa4941a6a30f41d9fc
MertArar <85193329+MertArar@users.noreply.github.com>
2023-06-05 05:01:44 +0300

    scripts part done

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,7 +1,75 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setTodoList,
+  addTodo,
+  updateTodo,
+  sortTodo,
+  toggleCompleted,
+} from "../store/Slice";
 
-const ToDoList = () => {
+import { TiPencil } from "react-icons/ti";
+import { BsTrash } from "react-icons/bs";
+import empty from "../assets/empty.jpeg";
+
+function ToDoList() {
+  const dispatch = useDispatch();
+  const todoList = useSelector((state) => state.todo.todoList);
+  const sortCriteria = useSelector((state) => state.todo.sortCriteria);
+  const [showModal, setShowModal] = useState(false);
+  const [currentTodo, setCurrentTodo] = useState(null);
+  const [newTask, setNewTask] = useState("");
+
+  useEffect(() => {
+    if (todoList.lenght > 0) {
+      localStorage.setItem("todoList", JSON.stringify(todoList));
+    }
+  }, [todoList]);
+
+  useEffect(() => {
+    const localTodoList = JSON.parse(localStorage.getItem("todoList"));
+    if (localTodoList) {
+      dispatch(setTodoList(localTodoList));
+    }
+  }, []);
+
+  const handleAddTodo = (task) => {
+    if (task.trim().lenght === 0) {
+      alert("Please enter a task");
+    } else {
+      dispatch(addTodo({ task: task, id: Date.now() }));
+      setNewTask("");
+      setShowModal(true);
+    }
+  };
+  const handleUpdateToDoList = (id, task) => {
+    if (task.trim().lenght === 0) {
+      alert("Please enter a task");
+    } else {
+      dispatch(updateTodo({ task: task, id: id }));
+      setShowModal(false);
+    }
+  };
+  const handleDeleteToDo = (id) => {
+    const updatedToDoList = todoList.filter((todo) => todo.id != id);
+    dispatch(setTodoList(updatedToDoList));
+    localStorage.setItem("todoList", JSON.stringify(updatedToDoList));
+  };
+  function handleSort(sortCriteria) {
+    dispatch(sortTodo(sortCriteria));
+  }
+
+  const sortToDoList = todoList.filter((todo) => {
+    if (sortCriteria === "All") return true;
+    if (sortCriteria === "Completed" && todo.completed) return true;
+    if (sortCriteria === "Not Completed" && !todo.completed) return true;
+    return false;
+  });
+
+  const handleToggleCompleted = (id) => {
+    dispatch(toggleCompleted({ id }));
+  };
   return <div>ToDoList</div>;
-};
+}
 
 export default ToDoList;
