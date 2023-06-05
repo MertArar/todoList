f42b27966ecb98949e40e53938e9e95b154e413f
MertArar <85193329+MertArar@users.noreply.github.com>
2023-06-05 05:16:23 +0300

    todoList section done

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -7,12 +7,10 @@ import {
   sortTodo,
   toggleCompleted,
 } from "../store/Slice";
-
 import { TiPencil } from "react-icons/ti";
 import { BsTrash } from "react-icons/bs";
 import empty from "../assets/empty.jpeg";
-
-function ToDoList() {
+function TodoList() {
   const dispatch = useDispatch();
   const todoList = useSelector((state) => state.todo.todoList);
   const sortCriteria = useSelector((state) => state.todo.sortCriteria);
@@ -21,11 +19,10 @@ function ToDoList() {
   const [newTask, setNewTask] = useState("");
 
   useEffect(() => {
-    if (todoList.lenght > 0) {
+    if (todoList.length > 0) {
       localStorage.setItem("todoList", JSON.stringify(todoList));
     }
   }, [todoList]);
-
   useEffect(() => {
     const localTodoList = JSON.parse(localStorage.getItem("todoList"));
     if (localTodoList) {
@@ -34,7 +31,7 @@ function ToDoList() {
   }, []);
 
   const handleAddTodo = (task) => {
-    if (task.trim().lenght === 0) {
+    if (task.trim().length === 0) {
       alert("Please enter a task");
     } else {
       dispatch(addTodo({ task: task, id: Date.now() }));
@@ -43,7 +40,7 @@ function ToDoList() {
     }
   };
   const handleUpdateToDoList = (id, task) => {
-    if (task.trim().lenght === 0) {
+    if (task.trim().length === 0) {
       alert("Please enter a task");
     } else {
       dispatch(updateTodo({ task: task, id: id }));
@@ -55,6 +52,7 @@ function ToDoList() {
     dispatch(setTodoList(updatedToDoList));
     localStorage.setItem("todoList", JSON.stringify(updatedToDoList));
   };
+
   function handleSort(sortCriteria) {
     dispatch(sortTodo(sortCriteria));
   }
@@ -69,7 +67,116 @@ function ToDoList() {
   const handleToggleCompleted = (id) => {
     dispatch(toggleCompleted({ id }));
   };
-  return <div>ToDoList</div>;
+  return (
+    <div>
+      {showModal && (
+        <div>
+          <div>
+            <input
+              value={newTask}
+              onChange={(e) => setNewTask(e.target.value)}
+              placeholder={
+                currentTodo ? "Update your task here" : "Enter your task here"
+              }
+            />
+            <div>
+              {currentTodo ? (
+                <>
+                  <button
+                    onClick={() => {
+                      setShowModal(false);
+                      handleUpdateToDoList(currentTodo.id, newTask);
+                    }}
+                  >
+                    Save
+                  </button>
+                  <button onClick={() => setShowModal(false)}>Cancel</button>
+                </>
+              ) : (
+                <>
+                  <button onClick={() => setShowModal(false)}>Cancel</button>
+                  <button
+                    onClick={() => {
+                      handleAddTodo(newTask);
+                      setShowModal(false);
+                    }}
+                  >
+                    Add
+                  </button>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+
+      <div>
+        {todoList.length === 0 ? (
+          <div>
+            <div>
+              <img src={empty} alt="" />
+            </div>
+            <p>You have no todo's, please add one.</p>
+          </div>
+        ) : (
+          <div>
+            <div>
+              <select onChange={(e) => handleSort(e.target.value)}>
+                <option value="All" className="text-sm">
+                  All
+                </option>
+                <option value="Completed" className="text-sm">
+                  Completed
+                </option>
+                <option value="Not Completed" className="text-sm">
+                  Not Completed
+                </option>
+              </select>
+            </div>
+            <div>
+              {sortToDoList.map((todo) => (
+                <div key={todo.id}>
+                  <div
+                    className={`${
+                      todo.completed
+                        ? "line-through text-greenTeal"
+                        : "text-sunsetOrange"
+                    }`}
+                    onClick={() => {
+                      handleToggleCompleted(todo.id);
+                    }}
+                  >
+                    {todo.task}
+                  </div>
+                  <div>
+                    <button
+                      onClick={() => {
+                        setShowModal(true);
+                        setCurrentTodo(todo);
+                        setNewTask(todo.task);
+                      }}
+                    >
+                      <TiPencil />
+                    </button>
+                    <button onClick={() => handleDeleteToDo(todo.id)}>
+                      <BsTrash />
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+        <button
+          onClick={() => {
+            setShowModal(true);
+          }}
+        >
+          Add Task
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default ToDoList;
+export default TodoList;
